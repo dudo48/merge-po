335dec3861527c213efdce8d852b0cadd68e914b
dudo48 <test>
2023-11-16 20:06:49 +0000

    added feature to remove unused references

--- mergepo.py
+++ mergepo.py
@@ -1,6 +1,8 @@
 import argparse
 import re
+
 import polib
+from termcolor import colored
 
 
 # checks if the given line is a reference gettext line
@@ -13,63 +15,98 @@ def occurrence_to_reference(occ):
     return '#: ' + (f'{occ[0]}:{occ[1]}' if occ[1] else occ[0])
 
 
-# returns occurrences that contain a certain model name
-def filter_match_regex(occurrences, regex):
-    return [occ for occ in occurrences if re.search(regex, occurrence_to_reference(occ))]
+# returns occurrences of the entry that match the regex
+def occurrences_match_regex(entry, regex):
+    return [occ for occ in entry.occurrences if re.search(regex, occurrence_to_reference(occ))]
 
 
 def merge_po(original_path, exported_path, output_path, regex, all_refs=False):
-    exported_model_entries_by_msgid = {}
+    exported_entry_by_msgid = {}
     for entry in polib.pofile(exported_path):
-        if filter_match_regex(entry.occurrences, regex):
+        matched_occurrences = occurrences_match_regex(entry, regex)
+        if matched_occurrences:
             if not all_refs:
-                entry.occurrences = filter_match_regex(entry.occurrences, regex)
-            exported_model_entries_by_msgid[entry.msgid] = entry
+                entry.occurrences = matched_occurrences
+            exported_entry_by_msgid[entry.msgid] = entry
+    original_entries_to_remove = {
+        e for e in polib.pofile(original_path) if occurrences_match_regex(e, regex) and e.msgid not in exported_entry_by_msgid
+    }
 
     # merge existing entries
-    total_lines_added = 0
-    merged_entries_count = 0
+    stats = {'lines_added': 0, 'lines_removed': 0, 'merged_entries': 0}
     merged_entries = set()
     duplicate_merged_entries = set()
     original_entry_by_line_num = {en.linenum: en for en in polib.pofile(original_path)}
     with open(output_path, 'w', encoding='utf-8') as out_f, open(original_path, 'r', encoding='utf-8') as orig_f:
-        line_num = 1
-        original_entry = None
         previous_line = ''
+        line_num = ref_index = 0
+        original_entry = exported_entry = exported_entry_occurrences = None
         for line in orig_f:
-            original_entry = original_entry_by_line_num.get(line_num, original_entry)
-            if original_entry and original_entry.msgid in exported_model_entries_by_msgid and is_reference(previous_line) and not is_reference(line):
-                exported_entry = exported_model_entries_by_msgid[original_entry.msgid]
-                new_occurrences = set(exported_entry.occurrences) - set(original_entry.occurrences)
-                for occ in new_occurrences:
-                    out_f.write(f'{occurrence_to_reference(occ)}\n')
-                if new_occurrences:
-                    print(f'Merged entry: \'{exported_entry.msgid}\': added {len(new_occurrences)} line(s)')
-                    merged_entries_count += 1
-                if exported_entry in merged_entries:
-                    duplicate_merged_entries.add(exported_entry)
-                merged_entries.add(exported_entry)
-                total_lines_added += len(new_occurrences)
-            out_f.write(line)
             line_num += 1
+            # assign entries
+            if line_num in original_entry_by_line_num:
+                original_entry = original_entry_by_line_num[line_num]
+                if original_entry.msgid in exported_entry_by_msgid:
+                    exported_entry = exported_entry_by_msgid[original_entry.msgid]
+                    exported_entry_occurrences = set(exported_entry.occurrences)
+                else:
+                    exported_entry = exported_entry_occurrences = None
+            # if we're inside a matched entry
+            if original_entry and exported_entry:
+                if is_reference(line):
+                    # do not write unused refs
+                    if (all_refs or re.search(regex, line)) and original_entry.occurrences[ref_index] not in exported_entry_occurrences:
+                        stats['lines_removed'] += 1
+                    else:
+                        out_f.write(line)
+                    ref_index += 1
+                # add new lines at the end
+                elif is_reference(previous_line):
+                    new_occurrences = set(exported_entry.occurrences) - set(original_entry.occurrences)
+                    for occ in new_occurrences:
+                        out_f.write(f'{occurrence_to_reference(occ)}\n')
+                    out_f.write(line)
+                    if new_occurrences:
+                        print(colored(f'Merged entry:  \'{exported_entry.msgid}\'', 'cyan'))
+                        stats['merged_entries'] += 1
+                    if exported_entry in merged_entries:
+                        duplicate_merged_entries.add(exported_entry)
+                    merged_entries.add(exported_entry)
+                    stats['lines_added'] += len(new_occurrences)
+                    ref_index = 0
+                else:
+                    out_f.write(line)
+            elif original_entry and original_entry in original_entries_to_remove:
+                stats['lines_removed'] += 1
+            else:
+                out_f.write(line)
             previous_line = line
+    
+    # log removed original entries
+    for entry in original_entries_to_remove:
+        print(colored(f'Removed entry: \'{entry.msgid}\'', 'red'))
 
     # add new entries at the bottom
-    new_entries = set([en for en in exported_model_entries_by_msgid.values() if en not in merged_entries])
+    new_entries = set([en for en in exported_entry_by_msgid.values() if en not in merged_entries])
     if new_entries:
         with open(output_path, 'a', encoding='utf-8') as out_f:
             out_f.write('\n')
             for entry in new_entries:
                 out_f.write(f'\n{str(entry)}')
-                lines_added = str(entry).count('\n')
-                print(f'Added entry: \'{entry.msgid}\': added {lines_added} line(s)')
-                total_lines_added += lines_added
+                print(colored(f'Added entry:   \'{entry.msgid}\'', 'green'))
+                stats['lines_added'] += str(entry).count('\n')
 
-    print(f'Added {len(new_entries)} entries and merged {merged_entries_count} entries: added {total_lines_added} line(s)')
+    
+    print(
+        'Added ' + colored(f'{len(new_entries)} entries', 'green')
+        + ', merged ' + colored(f'{stats["merged_entries"]} entries', 'cyan')
+        + ' and removed ' + colored(f'{len(original_entries_to_remove)} entries', 'red')
+    )
+    print('Added ' + colored(f'{stats["lines_added"]} line(s)', 'green') + ' and removed ' + colored(f'{stats["lines_removed"]} line(s)', 'red'))
 
     # warn about duplicates
     for entry in duplicate_merged_entries:
-        print(f'WARNING duplicate merged entry: \'{entry.msgid}\'')
+        print(colored(f'WARNING: duplicate merged entry: \'{entry.msgid}\'', 'yellow'))
 
 
 if __name__ == '__main__':
@@ -79,6 +116,6 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output-path', required=True, help='Output file path')
     parser.add_argument('-r', '--regex', required=True, help='Match only entries that have occurrences matching this regex')
     parser.add_argument('--all-refs', action='store_true', help='Whether to add all different references present in '
-                                                                'the exported file or add only those that contain the'
-                                                                ' specified model')
+                                                                'the exported file or add only those that match the'
+                                                                ' specified regex')
     merge_po(**vars(parser.parse_args()))
